f8b848c28798afacd581ebd7b876166355b63379
Nop3e3 <salma.q.qorashy@gmail.com>
2025-10-31 19:18:43 +0200

    newupd8

---
+++ src/page/Home.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './Home.css';
+import Navbar from '../components/layout/Navbar';
+import logo from '../Assets/logo.svg';
+export default function Home() {
+  return (
+    <div className="homee">
+<nav className='thenavbar'>
+  <img src={logo} alt="Logo" />
+  <div className='navelementss'>
+<Navbar label="About Us" />
+<Navbar label=" Investor Relations " />
+<Navbar label="Responsible Banking" />
+<Navbar label="Newsroom" />
+<Navbar label="Learning Center" />
+<Navbar label="Careers" />
+<Navbar label="Others" />
+</div>
+</nav>
+    </div>
+  );
+}
